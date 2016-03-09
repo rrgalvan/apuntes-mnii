3cda530596b7be0f19d1db5bf8014e022e3eadfc
Rafa Rodríguez Galván <rafael.rodriguez@uca.es>
2016-03-09 11:18:40 +0100

    Finalizado tema 1

--- tema1/src/biseccion-test1.py
+++ tema1/src/biseccion-test1.py
@@ -1,5 +1,4 @@
-def f(x): 
-    return x**3 - 9*x + 3
+f = lambda x: x**3 - 9*x + 3
 a, b = 0, 1
 res = biseccion(f, a, b, tol=1e-2, verb=True)
 print "Resultado: ", res
--- tema1/src/puntofijo-test1.py
+++ tema1/src/puntofijo-test1.py
@@ -1,4 +1,4 @@
 from math import *
-def g(x): return sqrt(sin(x)+1)
+g = lambda x: sqrt(sin(x)+1)
 res = punto_fijo(g, x0=1.0, tol=1e-5, verb=True)
 print "Resultado: ", res
